1890c8b4fe6ac622448691746180ab0be0bd85ed
lalitaalaalitah <46178760+lalitaalaalitah@users.noreply.github.com>
2021-07-26 02:14:56 +0530

    Update v1_FontTesting_26072021.py

--- v1_FontTesting_26072021.py
+++ v1_FontTesting_26072021.py
@@ -4,7 +4,7 @@ from itertools import permutations, islice, combinations
 import os
 
 
-list_a = [
+consonant_list = [
     "क",
     "ख",
     "ग",
@@ -25,7 +25,7 @@ list_a = [
     "द",
     "ध",
     "न",
-    "ऩ",
+    # "ऩ",
     "प",
     "फ",
     "ब",
@@ -39,20 +39,96 @@ list_a = [
     "श",
     "ष",
     "स",
-    "ह"
+    "ह",
 ]
 
+vowel_list = [
+    "अ",
+    "आ",
+    "इ",
+    "ई",
+    "उ",
+    "ऊ",
+    "ऋ",
+    "ॠ",
+    "ऌ",
+    "ॡ",
+    "ऍ",
+    "ऎ",
+    "ए",
+    "ऐ",
+    "ऑ",
+    "ऒ",
+    "ओ",
+    "औ",
+]
+
+mAtrA_list = [
+    "",
+    "ऀ",
+    "ँ",
+    "ं",
+    "ः",
+    "ऺ",
+    "ऻ",
+    # "़",
+    # "ऽ",
+    "ा",
+    "ि",
+    "ी",
+    "ु",
+    "ू",
+    "ृ",
+    "ॄ",
+    "ॢ",
+    "ॣ",
+    "ॅ",
+    "ॆ",
+    "े",
+    "ै",
+    "ॉ",
+    "ॊ",
+    "ो",
+    "ौ",
+    "्",
+    # "ॎ",
+    "ॏ",
+]
 # Use combinations
-for L in range(0, len(list_a)+1):
-    if L>1 and L < 6:
-        file_name = "permutations_of_" + str(L) + ".txt"
+for L in range(0, len(consonant_list) + 1):
+    if L > 1 and L < 6:
+        file_name = "combination_of_" + str(L) + ".txt"
+        print(file_name)
         # clear file
         with open(file_name, "w") as fn:
             pass
         with open(file_name, "a") as fn:
-            for subset in combinations(list_a, L):
-                print(subset)
-                item_joined = "्".join(subset)
-                fn.write(item_joined)
-                fn.write("\n")
-
+            for subset in combinations(consonant_list, L):
+                # print(subset)
+                # item_joined = "्".join(subset)
+                # fn.write(item_joined)
+                # fn.write("\n")
+                for each_mAtrA in mAtrA_list:
+                    item_joined = each_mAtrA.join(subset)
+                    item_joined_with_mAtrA = item_joined + each_mAtrA
+                    fn.write(item_joined_with_mAtrA)
+                    fn.write("\n")
+# use permutations
+for L in range(0, len(consonant_list) + 1):
+    if L > 1 and L < 6:
+        file_name = "permuation_of_" + str(L) + ".txt"
+        print(file_name)
+        # clear file
+        with open(file_name, "w") as fn:
+            pass
+        with open(file_name, "a") as fn:
+            for subset in permutations(consonant_list, L):
+                # print(subset)
+                # item_joined = "्".join(subset)
+                # fn.write(item_joined)
+                # fn.write("\n")
+                for each_mAtrA in mAtrA_list:
+                    item_joined = each_mAtrA.join(subset)
+                    item_joined_with_mAtrA = item_joined + each_mAtrA
+                    fn.write(item_joined_with_mAtrA)
+                    fn.write("\n")
