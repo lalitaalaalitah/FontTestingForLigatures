a65355432f6e891752fcc1c2d4d8151d2f711d30
lalitaalaalitah <46178760+lalitaalaalitah@users.noreply.github.com>
2021-11-14 11:50:54 +0530

    created script

--- v1_FontTesting_26072021.py
+++ v1_FontTesting_26072021.py
@@ -94,41 +94,32 @@ mAtrA_list = [
     # "ॎ",
     "ॏ",
 ]
-# Use combinations
-for L in range(0, len(consonant_list) + 1):
-    if L > 1 and L < 6:
-        file_name = "combination_of_" + str(L) + ".txt"
-        print(file_name)
-        # clear file
-        with open(file_name, "w") as fn:
-            pass
-        with open(file_name, "a") as fn:
-            for subset in combinations(consonant_list, L):
-                # print(subset)
-                # item_joined = "्".join(subset)
-                # fn.write(item_joined)
-                # fn.write("\n")
-                for each_mAtrA in mAtrA_list:
-                    item_joined = each_mAtrA.join(subset)
-                    item_joined_with_mAtrA = item_joined + each_mAtrA
-                    fn.write(item_joined_with_mAtrA)
-                    fn.write("\n")
-# use permutations
-for L in range(0, len(consonant_list) + 1):
-    if L > 1 and L < 6:
-        file_name = "permuation_of_" + str(L) + ".txt"
-        print(file_name)
-        # clear file
-        with open(file_name, "w") as fn:
-            pass
-        with open(file_name, "a") as fn:
-            for subset in permutations(consonant_list, L):
-                # print(subset)
-                # item_joined = "्".join(subset)
-                # fn.write(item_joined)
-                # fn.write("\n")
-                for each_mAtrA in mAtrA_list:
-                    item_joined = each_mAtrA.join(subset)
-                    item_joined_with_mAtrA = item_joined + each_mAtrA
-                    fn.write(item_joined_with_mAtrA)
-                    fn.write("\n")
+def per_n_com(permutations_or_combinations):
+    for L in range(0, len(consonant_list) + 1):
+        if L > 1 and L < 5:
+            if permutations_or_combinations == 'c':
+                file_name = "combinations_of_" + str(L) + ".txt"
+                bigData = combinations(consonant_list, L)
+            elif permutations_or_combinations == 'p':
+                file_name = "permuations_of_" + str(L) + ".txt"
+                bigData = permutations(consonant_list, L)
+            print(file_name)
+            # clear file
+            with open(file_name, "w") as fn:
+                pass
+            with open(file_name, "a") as fn:
+                for subset in bigData:
+                    # print(subset)
+                    # item_joined = "्".join(subset)
+                    # fn.write(item_joined)
+                    # fn.write("\n")
+                    for each_mAtrA in mAtrA_list:
+                        item_joined = each_mAtrA.join(subset)
+                        item_joined_with_mAtrA = item_joined + each_mAtrA
+                        fn.write(item_joined_with_mAtrA)
+                        fn.write("\n")
+
+# 
+if __name__ == "__main__":
+    per_n_com('c')
+    per_n_com('p')
